51c2b18e6a25cbdcf1f0e1b46ebbbe5fabb2495c
Edson Nascimento <aquenzitech@gmail.com>
2021-12-31 18:47:35 +1000

    small padding fix at receipt page

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -13,10 +13,10 @@ export default function Receipt() {
     paymentReceipt(order).then((data) => { setOrderData(data);console.log(data.table)})
   }, [])
   return (
-    <div className='p-5'>
+    <div className='py-5'>
       <h3 className='m-5 text-capitalize'>Hi {orderData && orderData.customer.username} !</h3>
       <h6 className='m-5'> Thanks for shopping at Sushme</h6>
-      <h6 className='m-5'>{orderData && orderData.table === "Online-Takeaway" ? null:`Your order is beeing prepared adn will be delivered to Table n.${orderData && orderData.table}, please wait.`}</h6>
+      <h6 className='m-5'>{orderData && orderData.table === "Online-Takeaway" ? null:`Your order is beeing prepared and will be delivered to Table n.${orderData && orderData.table} , please wait.`}</h6>
       
       <div className='my-5' >
         <a className='m-5 text-decoration-none text-black border p-3 rounded bg-light' href={orderData && orderData.payment} target={"_blank"} rel={"noopener noreferrer"} >Click here to see your receipt
